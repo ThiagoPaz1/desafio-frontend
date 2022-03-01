c661462bbadd64c1626af9456eff6fbc67fd75a3
Thiago da Paz <thiagopazjunior@gmail.com>
2022-03-01 12:22:01 -0300

    Melhorando o componente SearchBar

--- project-frontend/src/components/SearchBar.tsx
+++ project-frontend/src/components/SearchBar.tsx
@@ -3,9 +3,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function SearchBar() {
-  const [foundUser, setFoundUser]: any = useState();
+  const [foundUser, setFoundUser]: any = useState([]);
   const [userApi, setUserApi]: any = useState([]);
   const [loginProfile, setLoginProfile] = useState('');
+  const [message, setMessage] = useState('');
   
   useEffect(() => {
     if (loginProfile !== '' && loginProfile !== ' ') {
@@ -17,26 +18,45 @@ function SearchBar() {
 
   const handleChange = (event: any) => {
     const { value } = event.target;
-    
-    if (value !== '' && value !== ' ') {
-      setLoginProfile(value);
-    }
+    setLoginProfile(value);
   }
 
-  const searchProfiles = async (event: any) => {
+  const searchProfiles = (event: any) => {
     event.preventDefault();
-  
+    
     const filterUser = userApi.find((data: any) => data.login === loginProfile);
+    
+    let filterUserEquals;
 
-    setFoundUser(filterUser);
+    if (filterUser === undefined) {
+      setMessage('Perfil não encontrado');
+    }
+    if (filterUser !== undefined) {
+      filterUserEquals = foundUser.find((usersEquals: any) => usersEquals.login === filterUser.login)
+      
+      setMessage('');
+      setLoginProfile('');
+    }
+    if (filterUser !== undefined && !filterUserEquals) {
+      setFoundUser([...foundUser, filterUser]);
+      setLoginProfile('');
+    }
   }
 
   return ( 
     <div>
       <form>
-        <input onChange={ handleChange } type="text" placeholder="Digite um nome de perfil"/>
+        <input
+          onChange={ handleChange }
+          type="text" placeholder="Digite um nome de perfil"
+          value={ loginProfile }
+        />
         <button onClick={ searchProfiles }>Pesquisar</button>
       </form>
+      <h3>{ message }</h3>
+      <ul>
+        { foundUser.map((users:any, index:number) => <li key={ index }> {users.name}</li>)}
+      </ul>
     </div>
   );
 }
